c7a360718d0d79c124129f01fbbae63c5181c98f
Ariel Aguilar <ariel@goja.com>
2022-07-18 13:41:55 -0400

    App page removed

---
+++ src/views/DetailView.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style-sheets/DetailView.css';
+
+function DetailView() {
+
+  return (
+    <div className='detail-view'>
+      esta es mi pinche pagina detail
+    </div>
+  );
+}
+
+export default DetailView;
