5eaf8b0319c41376c24623ac21c257bdf377d69c
Ariel Aguilar <ariel@goja.com>
2022-07-22 14:15:19 -0400

    Added animation on comic-card hold

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Tooltip from '@material-ui/core/Tooltip';
+import General from '../mixins/GeneralFunctions'
 import '../style-sheets/HeroCard.css';
 
-function HeroCard({ props, name, customClickEvent }) {
+function HeroCard({ props, name, customClickEvent, type }) {
   const HtmlTooltip = withStyles((theme) => ({
     tooltip: {
       backgroundColor: '#f5f5f9',
@@ -14,11 +15,15 @@ function HeroCard({ props, name, customClickEvent }) {
     },
   }))(Tooltip);
   return (
-    <HtmlTooltip 
+    <HtmlTooltip
         title={ props.description ? <div className="hero-card__tooltip"><h4>{name}</h4><p>{props.description}</p></div> : <h4>{name}</h4> } placement="top" arrow enterDelay={400} enterNextDelay={400}>
-      <div className='hero-card' onClick={() => customClickEvent(props) }>
-        <img src={`${props.thumbnail.path}.${props.thumbnail.extension}`} onError={(e)=>{e.target.onerror = null; e.target.src=require('../assets/images/image_not_available.jpeg')}} className='hero-card__image' alt={ name } />
-        <div className='hero-card__name-label'> { name } </div>
+      <div className={[`${General.getTypeItem(type)}-card`, customClickEvent ? 'main-card' : '']} onClick={() => customClickEvent(props) }>
+        <span />
+        <span />
+        <span />
+        <span />
+        <img src={`${props.thumbnail.path}.${props.thumbnail.extension}`} onError={(e)=>{e.target.onerror = null; e.target.src=require('../assets/images/image_not_available.jpeg')}} className={`${General.getTypeItem(type)}-card__image`} alt={ name } />
+        <div className={`${General.getTypeItem(type)}-card__name-label`}> { name } </div>
       </div>
     </HtmlTooltip>
   );
--- src/mixins/GeneralFunctions.js
+++ src/mixins/GeneralFunctions.js
@@ -4,4 +4,11 @@ export default {
   capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   },
+
+  getTypeItem (string ){
+    if (string !== 'characters' && string !== 'creators') {
+      return 'comic';
+    }
+    return 'hero';
+  }
 }
